7853d8cc7d73d42f0516e383a354e17077e94514
Andrew Gaines <againes@mavs.coloradomesa.edu>
2017-09-21 11:16:51 -0600

    renamed package to legal name

---
+++ src/javafall2017/test/JavaFall2017Test.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafall2017.test;
+
+/**
+ *
+ * @author againes
+ */
+public class JavaFall2017Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //JavaFall2017Test app = new JavaFall2017Test();
+        //app.run();
+    }
+    
+}
