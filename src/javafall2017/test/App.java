1630b4ac823ad5b052c601bf8e549fa3b4f9f51a
Andrew Gaines <againes@mavs.coloradomesa.edu>
2017-09-26 12:05:41 -0600

    h2 tested

--- src/javafall2017/test/App.java
+++ src/javafall2017/test/App.java
@@ -5,7 +5,7 @@
  */
 package javafall2017.test;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  *
@@ -26,7 +26,97 @@ public class App {
        
     }
     
+    int h,w,n;
+    int[] x;
+    
+    class Lars
+    {
+     boolean annoyed = false;
+     boolean done = false;
+     int nextBrickHeight = 0;
+     int nextBrickOffset = 0;
+     int nextBrickIndex =0;
+     void work()
+     {
+         if(annoyed) return;
+         if(nextBrickIndex >=n)
+         {
+             annoyed = true;
+             return;
+         }
+         if(x[nextBrickIndex] + nextBrickOffset >w) {
+             annoyed = true;
+             return;
+         }
+         nextBrickOffset = nextBrickOffset +x[nextBrickIndex];
+         nextBrickIndex=nextBrickIndex+1;
+         if (nextBrickOffset ==w)
+         {
+             nextBrickOffset =0;
+             nextBrickHeight += 1;
+             if(nextBrickHeight ==h)
+             {
+                 done = true;
+             }
+         }       
+     }
+    }
+  
+    void readInput(Scanner in)
+    {
+        h=in.nextInt();
+        w=in.nextInt();
+        n=in.nextInt();
+        if (h < 1 || h > 100)
+        {
+            throw new IllegalStateException("invalid value of h: " +h);
+        }
+          if (w < 1 || h > 100)
+        {
+            throw new IllegalStateException("invalid value of w: " +w);
+        }
+            if (n < 1 || h > 10_000)
+        {
+            throw new IllegalStateException("invalid value of n: " +n);
+        }
+            x = new int[n];
+            for (int i=0;i<n;++i)
+            {
+                x[i]=in.nextInt();
+                  if (x[i] < 1 || x[i] > 10_000)
+        {
+            throw new IllegalStateException("invalid value of x: [" + i + "]: "+x);
+        }
+            }
+        
+    }
+    
+    boolean completed;
+    void solveProblem()
+    {
+        Lars lars= new Lars();
+        while (!lars.annoyed && !lars.done)
+        {
+            lars.work();
+        }
+        completed = lars.done;
+    }
+    
+    void writeOutput()
+    {
+        System.out.println(completed ? "YES" :"NO");
+    }
+    
+    void readInput()
+    {
+        Scanner in = new Scanner(System.in);
+        readInput(in);
+    }
+    
     void run() throws Exception {
-        System.out.println("args: " + Arrays.toString(args));
+        readInput();
+        solveProblem();
+        writeOutput();
+        
     }
 }
--- test/javafall2017/test/AppTest.java
+++ test/javafall2017/test/AppTest.java
@@ -5,6 +5,7 @@
  */
 package javafall2017.test;
 
+import java.util.Scanner;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -61,4 +62,59 @@ public class AppTest {
       
         assertArrayEquals(args,app.args);
     }
+
+    /**
+     * Test of readInput method, of class App.
+     */
+    @Test
+    public void testReadInput_Scanner() {
+        
+        System.out.println("readInput");
+        Scanner in = new Scanner("2 10 7\n" +
+                                 "4 5 5 5 5 5 9");
+        App app = new App(new String[]{});
+        app.readInput(in);
+        
+        assertEquals(2,app.h);
+        assertEquals(10,app.w);
+        assertEquals(7,app.n);
+        assertArrayEquals(new int[] {4,5,5,5,5,5,9}, app.x);
+    }
+
+    /**
+     * Test of solveProblem method, of class App.
+     */
+    @Test
+    public void testSolveProblem() {
+        
+        System.out.println("readInput");
+        Scanner in = new Scanner("2 10 7\n" +
+                                 "5 5 5 5 5 5 5");
+        App app = new App(new String[]{});
+        app.readInput(in);
+        app.solveProblem();
+        assertEquals(true, app.completed);
+        
+    }
+    
+     @Test
+    public void testSolveProblem2() {
+        
+        System.out.println("readInput");
+        Scanner in = new Scanner("2 10 7\n" +
+                                 "5 5 5 3 5 2 2");
+        App app = new App(new String[]{});
+        app.readInput(in);
+        app.solveProblem();
+        assertEquals(false, app.completed);
+        
+    }
+
+    /**
+     * Test of writeOutput method, of class App.
+     */
+    
+
+    
+    
 }
